aa6adf854116ea7cc9d76292493fec5d94cd01b9
document10 <popescul2222georgeteodor@gmail.com>
2025-03-04 19:48:30 +0200

    Add files via upload

---
+++ LAB2/Componenta.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public enum tip
+{
+    Generic,
+    Rezistor,
+    Condensator,
+    Bobina,
+    Tranzistor
+}
+
+public class Componenta
+{
+    string cod,unit;
+    tip tipc;
+    float val;
+
+    public Componenta(string _c="FARACOD",string _unit="X",tip _t=tip.Generic,float _val=0.0f)
+    {
+        cod = _c;
+        val = _val;
+        tipc = _t;
+        unit = _unit;
+    }
+
+    public string Afisare()
+    {
+        return $"{tipc.ToString()}, cod {cod}, {val} {unit}";
+    }
+
+}
---
+++ LAB2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProiectPIU
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Componenta c1=new Componenta(),c2 = new Componenta("1003","KOHMI",tip.Rezistor,100f);
+            Console.WriteLine(c1.Afisare());
+            Console.WriteLine(c2.Afisare());
+            CircuitIntegrat ci1 = new CircuitIntegrat(), ci2 = new CircuitIntegrat("1111", "Punte redresoare");
+            ci2.porturi.Add("IN+");
+            ci2.porturi.Add("IN-");
+            ci2.porturi.Add("OUT+");
+            ci2.porturi.Add("OUT-");
+            ci2.alteSpec.Add("Tensiune maxima 300V");
+            Console.WriteLine(ci1.Afisare());
+            Console.WriteLine(ci2.Afisare());
+        }
+    }
+}
